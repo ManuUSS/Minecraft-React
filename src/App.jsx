997b3b7ac51aab0dc869b3f1929161f5deaa62af
Manuel Ulate <fr3zs09@gmail.com>
2023-02-13 20:58:14 -0600

    Canvas and Sky

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Canvas } from '@react-three/fiber';
+import { Sky } from '@react-three/drei';
 
 function App() {
   
-
   return (
-    <h1>Hola</h1>
+    <Canvas>
+      <Sky></Sky>
+    </Canvas>
   )
 }
 
