8f11dd91fffb003bd2a34f2100c1e4e120534c0e
Manuel Ulate <fr3zs09@gmail.com>
2023-02-14 21:29:57 -0600

    creating cubes

--- src/components/Cubes.jsx
+++ src/components/Cubes.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { useStore } from '../hooks/useStore';
+import { Cube } from './Cube';
 
 export const Cubes = () => {
-  return (
-    <div>Cubes</div>
-  )
+    
+    const [ cubes ] = useStore( state => [ state.cubes ]);
+  
+    return cubes.map( ({ id, position, texture }) => (
+        <Cube key={ id } pos={ position } texture={ texture } />
+    ))
 }
